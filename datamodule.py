04780f2e7c1d88e47eb56bf53a11a03ab625a759
Sergey Andreev <sergey666andreev@gmail.com>
2020-11-10 16:30:09 +0600

    feat: Add DataModule

---
+++ datamodule.py
@@ -0,0 +1,39 @@
+import torch
+import pytorch_lightning as pl
+
+from torchvision import datasets
+from torchvision import transforms
+
+class FaceDataModule(pl.LightningDataModule):
+    def __init__(self, data_dir, batch_size, image_size=64, num_workers=0):
+        """Constructor
+
+        Args:
+            data_dir        Directory where images data is located
+            batch_size      The size of each batch; the number of images in a batch
+            image_size      The squaer size of the image data
+            num_workers     
+        """
+        super().__init__()
+
+        self.data_dir = data_dir
+        self.batch_size = batch_size
+        self.image_size = image_size
+        self.num_workers = num_workers
+
+    def train_dataloader(self):
+        transformation = transforms.Compose([
+            transforms.Resize(self.image_size),
+            transforms.ToTensor()
+        ])
+
+        dataset = datasets.ImageFolder(self.data_dir, transform=transformation)
+
+        data_loader = torch.utils.data.DataLoader(
+            dataset=dataset,
+            batch_size=self.batch_size,
+            shuffle=True,
+            num_workers=self.num_workers
+        )
+
+        return data_loader
